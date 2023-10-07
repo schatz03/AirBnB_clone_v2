e28e400865b834db0f9022fefb0ebfd60706d114
Schatz03 <nourisoukaina11@gmail.com>
2023-10-07 14:57:28 -0700

    airbnbv2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['52.91.124.43', '54.237.52.91']
+env.hosts = ['100.26.212.197', '100.24.235.200']
 
 
 def do_deploy(archive_path):
